93b5c988e60c5e55db5257a301cd161781c7af58
ryuta2727 <ryuta7260@icloud.com>
2021-12-10 11:14:32 +0900

    ライン風チャット画面

---
+++ ikimiti/Assets/Users/Shimizu/Chat/Test.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    //‚±‚êƒƒCƒ“ƒV[ƒ“‚Ì‚Ç‚Á‚©‚É“ü‚ê‚Æ‚¢‚Ä!
+    // Start is called before the first frame update
+    public void Testbutton()
+    {
+        SceneManager.UnloadScene("SubScene");
+    }
+}
