acd0b54bd42265c92f11bdeaad9b8f602615816c
Kuronakoyo <Kuronakoyo@gmail.com>
2021-12-13 11:51:18 +0900

    Update Enemy.cs

--- Assets/Script/EnemyScript/Enemy.cs
+++ Assets/Script/EnemyScript/Enemy.cs
@@ -13,20 +13,12 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    public void Cat()
-    {
-        _catImageObj.SetActive(true);
-    }
-    public void SP()
-    {
-        _suspiciousPersonImageObj.SetActive(true);
+
     }
 }
